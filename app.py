4459e4328cf5df656ac09f81c05683834e64c0f4
famesensor <famfew25@gmail.com>
2019-10-20 00:35:20 +0700

    unsuccessful

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import pymysql.cursors 
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
+@app.route('/upload',methods=["POST"])
+def upload():
+    file = request.files['inputFile']
+
+    return file.filename
 
 if __name__ == '__main__' :
-    app.run(dubug=True)
+    app.run(host='localhost',port=8000,debug=True)
